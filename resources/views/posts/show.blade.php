d67a2c100ec80d760554220f9a78c36a6665fb2d
Matko Đipalo <matkodjipalo@gmail.com>
2017-02-16 19:59:26 +0100

    comments controller

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,39 @@
 @extends('layouts.master')
 
 @section('content')
+	<div class="col-sm-8 blog-main">
+		<h1>{{ $post->title }}</h1>
 
-	<h1>Place to show the post</h1>
+		{{ $post->body }}
+
+		<hr/>
+
+		<div class="comments">
+			<ul class="list-group"> 
+	    		@foreach ($post->comments as $comment)
+	    			<li class="list-group-item">
+	    				<strong>{{ $comment->created_at->diffForHumans() }}: &nbsp</strong>
+	    				{{ $comment->body }}
+	    			</li>
+	    		@endforeach
+	    	</ul>
+    	</div>
+
+    	<hr/>
+
+    	<div class="card">
+    		<div class="card-block">
+    			<form method="POST" action="/posts/{{ $post->id }}/comment">
+    				<div class="form-group">
+    					<textarea name="body" placeholder="Your comment here." class="form-control"></textarea>
+    				</div>
+
+    				<div class="form-group">
+					  	<button type="submit" class="btn btn-primary">Add comment</button>
+					</div>
+    			</form>
+    		</div>
+    	</div>
+	</div>
 
 @endsection
